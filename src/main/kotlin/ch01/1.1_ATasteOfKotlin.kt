64f5509421f4f344e45eb3dc4cbbfa0decca3ba7
runningpig66 <runningpig66@gmail.com>
2025-10-13 12:09:00 +0800

    ch01

---
+++ src/main/kotlin/ch01/1.1_ATasteOfKotlin.kt
@@ -0,0 +1,23 @@
+package ch01.ex1_ATasteOfKotlin
+
+/**
+ * @author runningpig66
+ * @date 2025/10/13 周一
+ * @time 10:53
+ * 代码清单 P.4
+ * 代码清单 1.1 Kotlin 初体验
+ */
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(
+        Person("Alick"),
+        Person("Bob", age = 29),
+    )
+
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("The oldest is: $oldest")
+}
+/* Output:
+The oldest is: Person(name=Bob, age=29)
+ */
